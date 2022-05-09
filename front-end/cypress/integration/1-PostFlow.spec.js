090ec69bd1f65396f37fecd45b280cc28776e248
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-05-09 00:30:35 -0300

    feat: 2t2 test go to top and random page

--- front-end/cypress/integration/1-PostFlow.spec.js
+++ front-end/cypress/integration/1-PostFlow.spec.js
@@ -7,7 +7,7 @@ describe("POST: add song and vote", () => {
         cy.request("POST", "http://localhost:5000/tests/reset/dbTests");
     });
 
-    it("should post a song and vote on that", () => {
+    it("should post a song and vote on that ", () => {
         const name = faker.lorem.words(5);
         cy.visit("http://localhost:3000/");
 
@@ -28,5 +28,23 @@ describe("POST: add song and vote", () => {
         for (let i = 0; i < 2; i++)
             cy.get('.downVote').click();
         cy.get('.score').should("have.text", "3");
+    });
+});
+describe("Go to /top", () => {
+    it("should visit /top", () => {
+        cy.intercept("GET", "/recommendations/top/*").as("getTop");
+        cy.contains("Top").click();
+        cy.wait("@getTop");
+
+        cy.url().should("equal", "http://localhost:3000/top");
+    });
+});
+describe("Go to /random", () => {
+    it("should visit the pages correctly", () => {
+        cy.intercept("GET", "/recommendations/random").as("getRandom");
+        cy.contains("Random").click();
+        cy.wait("@getRandom");
+
+        cy.url().should("equal", "http://localhost:3000/random");
     });
 });
