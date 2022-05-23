7bed1f47fbc381bf8ee39bb51f97e6185f0f6d0e
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-05-23 05:10:20 -0300

    Preparando revisão de código

--- back-end/src/repositories/testsRepository.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../database.js";
-
-async function deleteAllDb() {
-  await prisma.recommendation.deleteMany({});
-}
-
-export default {
-  deleteAllDb,
-};
--- back-end/src/services/testsService.ts
+++
@@ -1,9 +0,0 @@
-import testsRepository from "../repositories/testsRepository.js";
-
-async function resetDbTests() {
-  await testsRepository.deleteAllDb();
-}
-
-export default {
-  resetDbTests,
-};
